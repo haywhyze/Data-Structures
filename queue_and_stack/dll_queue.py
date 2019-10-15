4538af68e2c180bd902cc31f59281deab74a9630
Yusuf Abdulkarim <lambda@Yusufs-MacBook-Pro.local>
2019-10-15 01:34:26 +0100

    implement enqueue and dequeue on dll_queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,22 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
-
+        # increase size of dll by 1
+        self.size +=1
+        # add value to tail of dll
+        self.storage.add_to_tail(value)
+        
     def dequeue(self):
-        pass
+        # check if dll is populated before dequeue
+        if self.size:
+            # if populated reduce length of dll and return first element
+            self.size -= 1
+            return self.storage.remove_from_head()
+        return None
+    
 
     def len(self):
-        pass
+        return self.size
