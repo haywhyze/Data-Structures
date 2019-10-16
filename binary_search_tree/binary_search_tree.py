95335305300a862fff0765717c77f4844390ba26
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-10-16 15:47:32 +0100

    implement contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -28,7 +28,19 @@ class BinarySearchTree:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        if target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        else:
+            if target > self.value:
+                if self.right:
+                    return self.right.contains(target)
+                else:
+                    return False
 
     # Return the maximum value found in the tree
     def get_max(self):
